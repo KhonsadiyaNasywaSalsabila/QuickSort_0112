50fc588c2e1af05b2b2911a538dd15b849b8ccee
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-03-29 11:11:28 +0700

    kondisi yang diulang

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -66,6 +66,11 @@ void q_short(int low, int high)
 
         while ((arr[j] > pivot) && (j >= low)) 
         {
+            j--; 
+            cmp_count++;
+        }
+        cmp_count++;
+
 
     
 

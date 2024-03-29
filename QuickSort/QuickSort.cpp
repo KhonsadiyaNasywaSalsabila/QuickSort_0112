8dcdf6bbb39fbbdce993d636990a0978b382bc54
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-03-29 11:03:17 +0700

    add untuk tampilan layar

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -20,4 +20,6 @@ void input() {
             cout << "\nMaksimum panjang array adalah 20" << endl;
     }
 
-
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;

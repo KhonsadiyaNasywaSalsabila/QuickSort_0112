4a70357ff511537524849088b2c2d5658878f56d
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-03-29 11:20:41 +0700

    menampilkan untuk jumlah langkah masing-masing variabel yang tertera

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -96,3 +96,7 @@ void display() {
     {
         cout << arr[i] << " ";
     }
+
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
